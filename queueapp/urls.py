f06aabe4c30aa218d47b151c03dd1d4446e071ac
Anthony Lopez <Antl21292@gmail.com>
2023-11-30 21:21:51 -0500

    fixed path

--- queueapp/urls.py
+++ queueapp/urls.py
@@ -4,5 +4,5 @@ from .views import HomePageView, BookMeView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('/bookme', BookMeView.as_view(), name='bookme')
+    path('bookme', BookMeView.as_view(), name='bookme'),
 ]
