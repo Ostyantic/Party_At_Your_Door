048c3f30dc85077d0eaacf3c3018857ad9e832dd
Anthony Lopez <Antl21292@gmail.com>
2023-12-05 00:16:39 -0500

    Created Singer model

--- queueapp/models.py
+++ queueapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Singer(models.Model):
+    name = models.CharField(max_length=20)
+    song_name = models.CharField(max_length=256)
 
 # Create your models here.
